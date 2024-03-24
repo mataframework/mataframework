2791e4861ae9da22222050ea56aa661a8c947e84
sibmaks <sibmaks@vk.com>
2024-03-24 17:49:18 +0300

    Clean up

--- src/test/kotlin/uitests/ui/IntroductionPage.kt
+++ src/test/kotlin/uitests/ui/IntroductionPage.kt
@@ -1,21 +1,13 @@
 package uitests.ui
 
-import io.appium.java_client.AppiumDriver
-import io.appium.java_client.MobileElement
 import io.appium.java_client.pagefactory.AndroidFindBy
 import io.appium.java_client.pagefactory.AppiumFieldDecorator
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.support.PageFactory
-import java.time.Duration
 
 class IntroductionPage(private val appiumFieldDecorator: AppiumFieldDecorator) {
-//    @CacheLookup
-//    @AndroidFindBy(xpath = "//android.view.ViewGroup[@resource-id=\"ru.kinopoisk:id/container\"]")
-//    private lateinit var contentContainerElement: WebElement
-//    @CacheLookup
     @AndroidFindBy(id = "ru.kinopoisk:id/description")
     private lateinit var descriptionElement: WebElement
-//    @CacheLookup
     @AndroidFindBy(id = "ru.kinopoisk:id/button_next")
     private lateinit var nextButtonElement: WebElement
 
