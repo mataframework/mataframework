2e184920cfb66ae0d0c74b5f2934f427f0138e8b
sibmaks <sibmaks@vk.com>
2024-06-03 02:13:22 +0300

    SPRINGTF-15 Интеграция GitHub WorkFlow

--- start_emu_headless.sh
+++ start_emu_headless.sh
@@ -9,24 +9,7 @@ NC='\033[0m' # No Color
 emulator_name=${EMULATOR_NAME}
 
 function check_hardware_acceleration() {
-    if [[ "$HW_ACCEL_OVERRIDE" != "" ]]; then
-        hw_accel_flag="$HW_ACCEL_OVERRIDE"
-    else
-        if [[ "$OSTYPE" == "darwin"* ]]; then
-            # macOS-specific hardware acceleration check
-            HW_ACCEL_SUPPORT=$(sysctl -a | grep -E -c '(vmx|svm)')
-        else
-            # generic Linux hardware acceleration check
-            HW_ACCEL_SUPPORT=$(grep -E -c '(vmx|svm)' /proc/cpuinfo)
-        fi
-
-        if [[ $HW_ACCEL_SUPPORT == 0 ]]; then
-            hw_accel_flag="-accel off"
-        else
-            hw_accel_flag="-accel on"
-        fi
-    fi
-
+    hw_accel_flag="-accel off"
     echo "$hw_accel_flag"
 }
 
@@ -34,15 +17,15 @@ hw_accel_flag=$(check_hardware_acceleration)
 
 function launch_emulator () {
   adb devices | grep emulator | cut -f1 | xargs -I {} adb -s "{}" emu kill
-  options="@${emulator_name} -no-window -no-snapshot -screen no-touch -noaudio -memory 2048 -no-boot-anim ${hw_accel_flag} -camera-back none"
+  options="@${emulator_name} -no-window -no-snapshot -screen no-touch -noaudio -memory 2048 -no-boot-anim ${hw_accel_flag} -camera-back none -gpu swiftshader_indirect"
   if [[ "$OSTYPE" == *linux* ]]; then
-    echo "${OSTYPE}: emulator ${options} -gpu off"
-    nohup $ANDROID_HOME/emulator/emulator $options -gpu off &
+    echo "${OSTYPE}: emulator ${options}"
+    nohup $ANDROID_HOME/emulator/emulator $options &
   fi
   if [[ "$OSTYPE" == *darwin* ]] || [[ "$OSTYPE" == *macos* ]]; then
     echo "im here"
-    echo "${OSTYPE}: emulator ${options} -gpu swiftshader_indirect"
-    nohup $ANDROID_HOME/emulator/emulator $options -gpu swiftshader_indirect &
+    echo "${OSTYPE}: emulator ${options}"
+    nohup $ANDROID_HOME/emulator/emulator $options &
   fi
 
   if [ $? -ne 0 ]; then
